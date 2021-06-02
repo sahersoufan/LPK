c20c79dc0ad9f7b86996577f4d13e9c81adec27e
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-06-02 13:55:15 +0300

    work in software clibs

--- Assets/generic/bars/bar2/bar2.cs
+++ Assets/generic/bars/bar2/bar2.cs
@@ -6,36 +6,56 @@ using UnityEngine.UI;
 public class bar2 : MonoBehaviour
 {
     public Sprite UP;
-    public GameObject bar;
-
+    public GameObject Bar;
+    public GameObject test;
 
     public up Up;
-    private void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("test");
-
-
 
         if (other.name == "up")
+        {
+
+            /*GameObject g = new GameObject();*/
+            GameObject g =  Instantiate(test);
+            g.AddComponent<RectTransform>();
+            g.GetComponent<RectTransform>().SetParent(Bar.transform);
+            g.transform.localScale = new Vector3((float)0.34, (float)0.11, (float)1);
+            g.GetComponent<RectTransform>().localPosition = new Vector3((float)-20, (float)37.5, (float)0);
+            /*   Image i = g.AddComponent<Image>();
+            i.sprite = UP;
+            g.GetComponent<RectTransform>().SetParent(Bar.transform);
+            g.layer = Bar.layer;
+            g.transform.position = new Vector3(Bar.transform.position.x, Bar.transform.position.y, Bar.transform.position.z);
+            g.transform.localScale = new Vector3((float) 0.5,(float) 0.5, (float) 0);
+            g.SetActive(true);*/
+        }
+        else if (other.name == "down")
         {
             GameObject g = new GameObject();
             Image i = g.AddComponent<Image>();
             i.sprite = UP;
-            g.GetComponent<RectTransform>().SetParent(bar.transform);
+            g.GetComponent<RectTransform>().SetParent(Bar.transform);
             g.SetActive(true);
-        }
-        else if (other.name == "down")
-        {
-
         }
         else if (other.name == "left")
         {
-
+            GameObject g = new GameObject();
+            Image i = g.AddComponent<Image>();
+            i.sprite = UP;
+            g.GetComponent<RectTransform>().SetParent(Bar.transform);
+            g.SetActive(true);
         }
         else if (other.name == "right")
         {
-
+            GameObject g = new GameObject();
+            Image i = g.AddComponent<Image>();
+            i.sprite = UP;
+            g.GetComponent<RectTransform>().SetParent(Bar.transform);
+            g.SetActive(true);
         }
+        /*
         else if (other.name == "rotateLeft")
         {
 
@@ -43,7 +63,7 @@ public class bar2 : MonoBehaviour
         else if (other.name == "rotateRight")
         {
 
-        }
+        }*/
 
     }
 
--- Assets/generic/programming something/up/up.cs
+++ Assets/generic/programming something/up/up.cs
@@ -7,14 +7,18 @@ public class up : MonoBehaviour
 
     public GameObject UP;
     static float x = 1;
-
+    static bool t = false;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(x,0,0);
+        if (t)
+        {
+            transform.position = new Vector3(x, 0, 0);
+        }
     }
     public static void size()
     {
         x = 300;
+        t = true;
     }
 }
