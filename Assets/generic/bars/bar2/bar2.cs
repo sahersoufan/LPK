1fc774bdfa4fad637c9cf9256aedd0a331545c41
sahersoufan <51461684+sahersoufan@users.noreply.github.com>
2021-06-04 16:03:40 +0300

    update bar2 script

--- Assets/generic/bars/bar2/bar2.cs
+++ Assets/generic/bars/bar2/bar2.cs
@@ -10,12 +10,14 @@ public class bar2 : MonoBehaviour
     private static BoxCollider2D boxCollider;
     private static GameObject stBar;
     private static GameObject stUp;
-    GameObject[] a;
-    public  GameObject Bar;
+    private static ArrayList objects = new ArrayList();
+    public GameObject Bar;
     public GameObject upInBar;
     public GameObject downInBar;
     public GameObject leftInBar;
     public GameObject rightInBar;
+
+    private static int counter4SoftwreClibs = 0;
 
 
 
@@ -31,17 +33,50 @@ public class bar2 : MonoBehaviour
 
     public static void addUp()
     {
-        GameObject g = Instantiate(stUp);
-        g.AddComponent<RectTransform>();
-        g.GetComponent<RectTransform>().SetParent(stBar.transform);
-        g.transform.localScale = new Vector3((float)0.34, (float)0.11, (float)1);
-        /*        GameObject[] objects =  FindGameObjectsInLayer(6
-        */
-        g.layer = 6;
-        g.GetComponent<RectTransform>().localPosition = new Vector3((float)-20, (float)37.5, (float)0);
+        if (counter4SoftwreClibs < 11)
+        {
+            GameObject g = Instantiate(stUp);
+            g.AddComponent<RectTransform>();
+            g.GetComponent<RectTransform>().SetParent(stBar.transform);
+            g.transform.localScale = new Vector3((float)0.34, (float)0.11, (float)1);
+            GameObject previos = getFromObjects(objects.Count - 1);
+            if (objects != null)
+            {
+                g.name = "p" + counter4SoftwreClibs;
+                counter4SoftwreClibs++;
+                g.layer = 6;
+                addtoObjects(g);
+                /*            Debug.Log(objects[objects.Length - 1].name);*/
+
+                float x = previos.GetComponent<RectTransform>().localPosition.x;
+                float y = previos.GetComponent<RectTransform>().localPosition.y;
+                float z = previos.GetComponent<RectTransform>().localPosition.z;
+                g.GetComponent<RectTransform>().localPosition = new Vector3((float)x, (float)y - 7, (float)z);
+
+            }
+            else
+            {
+                g.name = "p" + counter4SoftwreClibs;
+                counter4SoftwreClibs++;
+                g.layer = 6;
+                addtoObjects(g);
+                g.GetComponent<RectTransform>().localPosition = new Vector3((float)-20, (float)37.5, (float)0);
+
+            }
+
+
+        }
     }
 
+    private static void addtoObjects(GameObject obj)
+    {
+        objects.Add(obj);
+    }
 
+    private static GameObject getFromObjects(int index)
+    {
+        return (GameObject)objects[index];
+    }
     static GameObject[] FindGameObjectsInLayer(int layer)
     {
         var goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
